46d096cb77f1e88aed1f19cb6740830611362306
diversemix <diversemix@gmail.com>
2023-07-27 10:15:45 +0100

    add health endpoint

--- app/main.py
+++ app/main.py
@@ -2,13 +2,19 @@ from fastapi_pagination import add_pagination
 from app.logging_config import DEFAULT_LOGGING, setup_json_logging
 from app.api.api_v1.routers import families_router
 from fastapi import FastAPI
+from fastapi_health import health
 import uvicorn
+
+from app.service.health import is_database_online
 
 
 app = FastAPI(title="navigator-admin")
 setup_json_logging(app)
 add_pagination(app)
 app.include_router(families_router, prefix="/api/v1", tags=["families"])
+
+# add health endpoint
+app.add_api_route("/health", health([is_database_online]))
 
 
 @app.get("/api/v1", include_in_schema=False)
---
+++ app/service/health.py
@@ -0,0 +1,11 @@
+from app.db.session import get_db
+
+
+def is_database_online() -> bool:
+    """
+    Checks database health.
+
+    TODO: More comprehensive health checks
+    """
+
+    return get_db() is not None
