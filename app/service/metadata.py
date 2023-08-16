b27059ab5bc93620b78557a0e2abb42732fbe636
diversemix <diversemix@gmail.com>
2023-08-16 11:58:36 +0100

    implement metadata validation

--- app/service/metadata.py
+++ app/service/metadata.py
@@ -4,6 +4,25 @@ from sqlalchemy.orm import Session
 import app.repository.metadata as metadata_repo
 
 
+KEY_ALLOW_ANY = "allow_any"
+KEY_ALLOW_BLANKS = "allow_blanks"
+KEY_ALLOWED_VALUES = "allowed_values"
+
+"""
+Behaviour:
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~...
+| allow_any | allow_blanks  | Behaviour
+---------------------------------------...
+| True      | True          | allowed_values is ignored and anything goes
+| True      | False         | allowed_values is ignored but you need a non-blank value
+| False     | True          | allowed_values is used to validate, blank is also valid
+| False     | False         | allowed_values is used to validate, blank is invalid
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~...
+"""
+VALID_SCHEMA_KEYS = [KEY_ALLOW_ANY, KEY_ALLOW_BLANKS, KEY_ALLOWED_VALUES]
+
+
 def validate(db: Session, org_id: int, data: Json) -> bool:
     schema = metadata_repo.get_schema_for_org(db, org_id)
     if schema is None:
@@ -18,5 +37,77 @@ def _validate(schema: Json, data: Json) -> bool:
     :param Json schema: The schema to use for the validation
     :param Json data: The values to use
     :return bool: True if valid, False if not
+    :raises ValidationError: _description_
     """
+    # TODO : use pydantic
+
+    # Quick schema validation first
+    valid_data_keys = _validate_schema(schema)
+
+    missing_data_keys = set(valid_data_keys).difference(set(data.keys()))
+
+    if len(missing_data_keys) > 0:
+        raise ValidationError(
+            f"Values for the following are missing: {missing_data_keys}"
+        )
+
+    _validate_data(schema, data, valid_data_keys)
+
     return True
+
+
+def _validate_data(schema, data, valid_data_keys):
+    for key, value in data.items():
+        # First check key is valid against those in the schema
+        if key not in valid_data_keys:
+            raise ValidationError(f"Unknown '{key}' not in {valid_data_keys}")
+
+        key_schema = schema[key]
+
+        allow_blanks = (
+            True
+            if KEY_ALLOW_BLANKS in key_schema and key_schema[KEY_ALLOW_BLANKS] is True
+            else False
+        )
+
+        if value == "":
+            if allow_blanks:
+                continue
+            raise ValidationError(f"Value for {key} is blank, and that is not allowed")
+
+        allow_any = (
+            True
+            if KEY_ALLOW_ANY in key_schema and key_schema[KEY_ALLOW_ANY] is True
+            else False
+        )
+
+        if allow_any:
+            continue
+
+        valid_values = key_schema[KEY_ALLOWED_VALUES]
+        if value not in valid_values:
+            raise ValidationError(
+                f"Value '{value}' is not in the allowed list: {valid_values}"
+            )
+
+
+def _validate_schema(schema):
+    valid_data_keys = []
+    for key, value in schema.items():
+        valid_data_keys.append(key)
+
+        # Check the schema has the values we expect
+        values = [k for k in value.keys()]
+        unknown_values = set(values).difference(set(VALID_SCHEMA_KEYS))
+
+        # Raise if any unknown ones
+        if len(unknown_values) > 0:
+            raise ValueError(f"Unknown values in schema: {unknown_values}")
+
+        # TODO: The following is too simplistic as certain sub-sets are valid
+        # missing_values = set(VALID_SCHEMA_KEYS).difference(set(values))
+        # # Raise if any missing ones
+        # if len(missing_values) > 0:
+        #     raise ValueError(f"Missing values in schema: {missing_values}")
+
+    return valid_data_keys
---
+++ unit_tests/service/test_metadata_service.py
@@ -0,0 +1,125 @@
+import json
+
+import pytest
+from app.errors.validation_error import ValidationError
+from app.service.metadata import _validate
+
+
+SCHEMA_VALUES_NO_BLANKS = json.loads(
+    """
+{
+  "author": {
+    "allow_blanks": false,
+    "allowed_values": ["peter", "joel", "mark"]
+  }
+}
+"""
+)
+
+SCHEMA_VALUES_BLANKS = json.loads(
+    """
+{
+  "author": {
+    "allow_blanks": true,
+    "allowed_values": ["peter", "joel", "mark"]
+  }
+}
+"""
+)
+
+SCHEMA_ANY_NO_BLANKS = json.loads(
+    """
+{
+  "author": {
+    "allow_any": true,
+    "allow_blanks": false,
+    "allowed_values": []
+  }
+}
+"""
+)
+
+SCHEMA_ANY_BLANKS = json.loads(
+    """
+{
+  "author": {
+    "allow_any": true,
+    "allow_blanks": true,
+    "allowed_values": []
+  }
+}
+"""
+)
+
+
+# ----- PASSES
+
+
+def test_metadata_good_when_any_and_allow_blanks():
+    assert _validate(SCHEMA_ANY_BLANKS, {"author": "henry"})
+    assert _validate(SCHEMA_ANY_BLANKS, {"author": ""})
+
+
+def test_metadata_good_when_values_and_allow_blanks():
+    assert _validate(SCHEMA_VALUES_BLANKS, {"author": "peter"})
+    assert _validate(SCHEMA_VALUES_BLANKS, {"author": ""})
+
+
+def test_metadata_good_when_any_and_no_blanks():
+    assert _validate(SCHEMA_ANY_NO_BLANKS, {"author": "henry"})
+
+
+def test_metadata_good_when_values_and_no_blanks():
+    assert _validate(SCHEMA_VALUES_NO_BLANKS, {"author": "peter"})
+
+
+def test_metadata_good_when_blank_schema_and_data():
+    assert _validate({}, {})
+
+
+# ----- FAILS
+
+
+def test_metadata_fails_when_no_data():
+    with pytest.raises(ValidationError) as e:
+        _validate(SCHEMA_ANY_BLANKS, {})
+    expected_msg = "Values for the following are missing: {'author'}"
+    assert e.value.message == expected_msg
+
+
+def test_metadata_fails_when_invalid_key():
+    with pytest.raises(ValidationError) as e:
+        _validate(SCHEMA_ANY_BLANKS, {"Something": "value", "author": "peter"})
+    expected_msg = "Unknown 'Something' not in ['author']"
+    assert e.value.message == expected_msg
+
+
+def test_metadata_fails_when_any_and_allow_blanks():
+    # This should never fail
+    pass
+
+
+def test_metadata_fails_when_values_and_allow_blanks():
+    with pytest.raises(ValidationError) as e:
+        _validate(SCHEMA_VALUES_BLANKS, {"author": "henry"})
+    expected_msg = "Value 'henry' is not in the allowed list: ['peter', 'joel', 'mark']"
+    assert e.value.message == expected_msg
+
+
+def test_metadata_fails_any_and_no_blanks():
+    with pytest.raises(ValidationError) as e:
+        _validate(SCHEMA_ANY_NO_BLANKS, {"author": ""})
+    expected_msg = "Value for author is blank, and that is not allowed"
+    assert e.value.message == expected_msg
+
+
+def test_metadata_fails_values_and_no_blanks():
+    with pytest.raises(ValidationError) as e:
+        _validate(SCHEMA_VALUES_NO_BLANKS, {"author": "henry"})
+    expected_msg = "Value 'henry' is not in the allowed list: ['peter', 'joel', 'mark']"
+    assert e.value.message == expected_msg
+
+    with pytest.raises(ValidationError) as e:
+        _validate(SCHEMA_VALUES_NO_BLANKS, {"author": ""})
+    expected_msg = "Value for author is blank, and that is not allowed"
+    assert e.value.message == expected_msg
