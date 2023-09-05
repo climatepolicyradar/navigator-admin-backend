a730998420d5f3998bbdb8e4641fcfbe3964c835
diversemix <diversemix@gmail.com>
2023-09-05 12:25:58 +0100

    add token_service and tests

---
+++ app/service/token.py
@@ -0,0 +1,79 @@
+import jwt
+import os
+from datetime import datetime, timedelta
+from typing import Any, Optional
+from app.errors.token_error import TokenError
+
+from app.model.jwt_user import JWTUser
+
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = "HS256"
+# TODO: revisit/configure access token expiry
+ACCESS_TOKEN_EXPIRE_MINUTES = 24 * 60  # 24 hours for access token
+
+
+def encode(
+    email: str, is_superuser: bool, authorisation: dict, minutes: Optional[int] = None
+) -> str:
+    """
+    Encodes the user's data into a JWT token.
+
+    :param str email: User's email
+    :param bool is_superuser: User's ability to be a superuser.
+    :param dict authorisation: Authorisation information (not yet fully spec'd)
+    :param Optional[int] minutes: TTL for the token
+    :return str: The encoded token
+    """
+
+    if "@" not in email or "." not in email:
+        raise TokenError(f"Parameter email should be an email, not {email}")
+
+    if type(authorisation) != dict:
+        raise TokenError(
+            f"Parameter authorisation should be a dict, not {authorisation}"
+        )
+
+    if type(is_superuser) != bool:
+        raise TokenError(f"Parameter is_superuser should be a bool, not {is_superuser}")
+
+    to_encode = {
+        "sub": email,
+        "email": email,
+        "is_superuser": is_superuser,
+        "authorisation": authorisation,
+    }
+    expiry_minutes = minutes or ACCESS_TOKEN_EXPIRE_MINUTES
+    expire = datetime.utcnow() + timedelta(minutes=expiry_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def decode(token: str) -> JWTUser:
+    """
+    Decodes the JWT token into a JWTUser.
+
+    :param str token: The token to decode.
+    :raises TokenError: If cannot decode the string
+    :raises TokenError: If the decoded token does not contain the necessary
+    information to create a JWTUser.
+    :return JWTUser: The decoded user.
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except jwt.PyJWTError:
+        raise TokenError("Payload cannot be decoded")
+
+    email: Optional[str] = payload.get("email")
+    if email is None:
+        raise TokenError("Token did not contain an email")
+
+    authorisation: Optional[dict[str, Any]] = payload.get("authorisation")
+
+    jwt_user = JWTUser(
+        email=email,
+        is_superuser=payload.get("is_superuser", False),
+        authorisation=authorisation,
+    )
+    return jwt_user
---
+++ unit_tests/service/token_service.py
@@ -0,0 +1,66 @@
+from typing import cast
+import jwt
+import pytest
+from app.errors.token_error import TokenError
+import app.service.token as token_service
+
+
+@pytest.mark.parametrize(
+    "email",
+    ["e1@here.com", "e2@there.com", "e3@nowhere.com"],
+)
+@pytest.mark.parametrize(
+    "is_superuser",
+    [False, True, False],
+)
+@pytest.mark.parametrize(
+    "authorisation",
+    [{}, {"e2": False}, {"e3": {"a": 1}}],
+)
+def test_token_encoded_and_decoded(email: str, is_superuser: bool, authorisation: dict):
+    token = token_service.encode(email, is_superuser, authorisation)
+    assert token is not None
+    user = token_service.decode(token)
+    assert user.email == email
+    assert user.is_superuser == is_superuser
+    assert user.authorisation == authorisation
+
+
+def test_encode_checks_authorisation():
+    with pytest.raises(TokenError) as e:
+        token_service.encode("email@here.com", False, cast(dict, "random stuff"))
+
+    assert (
+        e.value.message == "Parameter authorisation should be a dict, not random stuff"
+    )
+
+
+def test_encode_checks_email():
+    with pytest.raises(TokenError) as e:
+        token_service.encode("email.here.com", False, {})
+
+    assert e.value.message == "Parameter email should be an email, not email.here.com"
+
+
+def test_encode_checks_is_superuser():
+    with pytest.raises(TokenError) as e:
+        token_service.encode("email@here.com", cast(bool, "False"), {})
+
+    assert e.value.message == "Parameter is_superuser should be a bool, not False"
+
+
+def test_decode_fails_when_empty():
+    with pytest.raises(TokenError) as e:
+        token_service.decode("")
+
+    assert e.value.message == "Payload cannot be decoded"
+
+
+def test_decode_fails_when_no_email():
+    encoded_jwt = jwt.encode(
+        {}, token_service.SECRET_KEY, algorithm=token_service.ALGORITHM
+    )
+    with pytest.raises(TokenError) as e:
+        token_service.decode(encoded_jwt)
+
+    assert e.value.message == "Token did not contain an email"
