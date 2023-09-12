712b0a2ae9f89dc12f0575d5755c61ac7e56444b
diversemix <diversemix@gmail.com>
2023-09-12 11:29:03 +0100

    complete the collection repo

--- app/repository/collection.py
+++ app/repository/collection.py
@@ -6,37 +6,50 @@ from typing import Optional, Tuple, cast
 from sqlalchemy.orm import Session
 from app.db.models.app.users import Organisation
 from app.db.models.law_policy.collection import CollectionFamily, CollectionOrganisation
-from app.db.models.law_policy.family import Family
+from app.db.models.law_policy.family import Family, Slug
+from app.errors import RepositoryError
 from app.model.collection import CollectionDTO
 from app.db.models.law_policy import Collection
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Query
 
+from sqlalchemy import or_, update as db_update, delete as db_delete
+from sqlalchemy_utils import escape_like
+
+from app.repository.helpers import generate_slug
 
 _LOGGER = logging.getLogger(__name__)
 
 CollectionOrg = Tuple[Collection, Organisation]
 
-# __        _____ ____
-# \ \      / /_ _|  _ \
-#  \ \ /\ / / | || |_) |
-#   \ V  V /  | ||  __/
-#    \_/\_/  |___|_|
 
-# def _collection_org_from_dto(
-#     dto: CollectionDTO, geo_id: int, org_id
-# ) -> Tuple[Collection, Organisation]:
-#     return (
-#         Collection(
-#             import_id=dto.import_id,
-#             title=dto.title,
-#             description=dto.summary,
-#             geography_id=geo_id,
-#             collection_category=dto.category,
-#         ),
-#         CollectionOrganisation(collection_import_id=dto.import_id,
-#   organisation_id=org_id),
-#     )
+def _collection_org_from_dto(
+    dto: CollectionDTO, org_id: int
+) -> Tuple[Collection, Organisation]:
+    return (
+        Collection(
+            import_id=dto.import_id,
+            title=dto.title,
+            description=dto.description,
+        ),
+        CollectionOrganisation(
+            collection_import_id=dto.import_id, organisation_id=org_id
+        ),
+    )
+
+
+def _get_query(db: Session) -> Query:
+    # NOTE: SqlAlchemy will make a complete hash of the query generation
+    #       if columns are used in the query() call. Therefore, entire
+    #       objects are returned.
+    return (
+        db.query(Collection, Organisation)
+        .join(
+            CollectionOrganisation,
+            CollectionOrganisation.collection_import_id == Collection.import_id,
+        )
+        .join(Organisation, Organisation.id == CollectionOrganisation.organisation_id)
+    )
 
 
 def _collection_to_dto(db: Session, co: CollectionOrg) -> CollectionDTO:
@@ -56,62 +69,13 @@ def _collection_to_dto(db: Session, co: CollectionOrg) -> CollectionDTO:
     )
 
 
-# def _generate_slug(
-#     title: str,
-#     lookup: set[str],
-#     attempts: int = 100,
-#     suffix_length: int = 4,
-# ):
-#     base = slugify(str(title))
-#     # TODO: try to extend suffix length if attempts are exhausted
-#     suffix = str(uuid4())[:suffix_length]
-#     count = 0
-#     while (slug := f"{base}_{suffix}") in lookup:
-#         count += 1
-#         suffix = str(uuid4())[:suffix_length]
-#         if count > attempts:
-#             raise RuntimeError(
-#                 f"Failed to generate a slug for {base} after {attempts} attempts."
-#             )
-#     lookup.add(slug)
-#     return slug
-
-
-# def _update_intention(db, collection, geo_id, original_collection):
-#     update_title = cast(str, original_collection.title) != collection.title
-#     update_basics = (
-#         update_title
-#         or original_collection.description != collection.summary
-#         or original_collection.geography_id != geo_id
-#         or original_collection.collection_category != collection.category
-#     )
-#     existing_metadata = (
-#         db.query(CollectionMetadata)
-#         .filter(CollectionMetadata.collection_import_id == collection.import_id)
-#         .one()
-#     )
-#     update_metadata = existing_metadata.value != collection.metadata
-#     return update_title, update_basics, update_metadata
-
-
-def _collection_query(db: Session) -> Query:
-    return (
-        db.query(Collection, Organisation)
-        .join(
-            CollectionOrganisation,
-            CollectionOrganisation.collection_import_id == Collection.import_id,
-        )
-        .join(Organisation, Organisation.id == CollectionOrganisation.organisation_id)
-    )
-
-
 def all(db: Session) -> list[CollectionDTO]:
     """
     Returns all the collections.
 
     :return Optional[CollectionResponse]: All of things
     """
-    collections = _collection_query(db).all()
+    collections = _get_query(db).all()
 
     if not collections:
         return []
@@ -129,9 +93,7 @@ def get(db: Session, import_id: str) -> Optional[CollectionDTO]:
     :return Optional[CollectionResponse]: A single collection or nothing
     """
     try:
-        collection_org = (
-            _collection_query(db).filter(Collection.import_id == import_id).one()
-        )
+        collection_org = _get_query(db).filter(Collection.import_id == import_id).one()
     except NoResultFound as e:
         _LOGGER.error(e)
         return
@@ -139,162 +101,109 @@ def get(db: Session, import_id: str) -> Optional[CollectionDTO]:
     return _collection_to_dto(db, collection_org)
 
 
-# def search(db: Session, search_term: str) -> list[CollectionDTO]:
-#     """
-#     Gets a list of collections from the repository searching title and summary.
+def search(db: Session, search_term: str) -> list[CollectionDTO]:
+    """
+    Gets a list of collections from the repository searching title and summary.
 
-#     :param str search_term: Any search term to filter on title or summary
-#     :return Optional[list[CollectionResponse]]: A list of matches
-#     """
-#     term = f"%{escape_like(search_term)}%"
-#     search = or_(Collection.title.ilike(term), Collection.description.ilike(term))
-#     found = _fam_geo_meta_query(db).filter(search).all()
+    :param str search_term: Any search term to filter on title or summary
+    :return Optional[list[CollectionResponse]]: A list of matches
+    """
+    term = f"%{escape_like(search_term)}%"
+    search = or_(Collection.title.ilike(term), Collection.description.ilike(term))
+    found = _get_query(db).filter(search).all()
 
-#     return [_collection_to_dto(db, f) for f in found]
-
-
-# def update(db: Session, collection: CollectionDTO, geo_id: int) -> bool:
-#     """
-#     Updates a single entry with the new values passed.
-
-#     :param str import_id: The collection import id to change.
-#     :param CollectionDTO collection: The new values
-#     :param int geo_id: a validated geography id
-#     :return Optional[CollectionDTO]: The new values set or None if not found.
-#     """
-#     new_values = collection.dict()
-
-#     original_collection = (
-#         db.query(Collection).filter(Collection.import_id == collection.import_id)
-# .one_or_none()
-#     )
-
-#     if original_collection is None:  # Not found the collection to update
-#         _LOGGER.error(f"Unable to find collection for update {collection}")
-#         return False
-
-#     # Now figure out the intention of the request:
-#     update_title, update_basics, update_metadata = _update_intention(
-#         db, collection, geo_id, original_collection
-#     )
-
-#     # Return if nothing to do
-#     if not update_title and not update_basics and not update_metadata:
-#         return True
-
-#     # Update basic fields
-#     if update_basics:
-#         result = db.execute(
-#             db_update(Collection)
-#             .where(Collection.import_id == collection.import_id)
-#             .values(
-#                 title=new_values["title"],
-#                 description=new_values["summary"],
-#                 geography_id=geo_id,
-#                 collection_category=new_values["category"],
-#             )
-#         )
-#         if result.rowcount == 0:  # type: ignore
-#             msg = "Could not update collection fields: {collection}"
-#             _LOGGER.error(msg)
-#             raise RepositoryError(msg)
-
-#     # Update if metadata is changed
-#     if update_metadata:
-#         md_result = db.execute(
-#             db_update(CollectionMetadata)
-#             .where(CollectionMetadata.collection_import_id == collection.import_id)
-#             .values(value=collection.metadata)
-#         )
-#         if md_result.rowcount == 0:  # type: ignore
-#             msg = (
-#                 "Could not update the metadata for collection: "
-#                 + f"{collection.import_id} to {collection.metadata}"
-#             )
-#             _LOGGER.error(msg)
-#             raise RepositoryError(msg)
-
-#     # update slug if title changed
-#     if update_title:
-#         db.flush()
-#         lookup = set([cast(str, n) for n in db.query(Slug.name).all()])
-#         name = _generate_slug(collection.title, lookup)
-#         new_slug = Slug(
-#             collection_import_id=collection.import_id,
-#             collection_document_import_id=None,
-#             name=name,
-#         )
-#         db.add(new_slug)
-#         _LOGGER.info(f"Added a new slug for {collection.import_id}
-# of {new_slug.name}")
-
-#     return True
+    return [_collection_to_dto(db, f) for f in found]
 
 
-# def create(
-#     db: Session, collection: CollectionDTO, geo_id: int, org_id: int
-# ) -> Optional[CollectionDTO]:
-#     """
-#     Creates a new collection.
+def update(db: Session, collection: CollectionDTO) -> bool:
+    """
+    Updates a single entry with the new values passed.
 
-#     :param CollectionDTO collection: the values for the new collection
-#     :param int geo_id: a validated geography id
-#     :return Optional[CollectionDTO]: the new collection created
-#     """
-#     try:
-#         new_collection, new_fam_org = _collection_org_from_dto(collection,
-# geo_id, org_id)
-#         db.add(new_collection)
-#         db.add(new_fam_org)
-#     except Exception as e:
-#         _LOGGER.error(e)
-#         return
+    :param str import_id: The collection import id to change.
+    :param CollectionDTO collection: The new values
+    :return bool: True if new values were set otherwise false.
+    """
+    new_values = collection.dict()
 
-#     # Add a slug
-#     lookup = set([cast(str, n) for n in db.query(Slug.name).all()])
-#     db.add(
-#         Slug(
-#             collection_import_id=collection.import_id,
-#             collection_document_import_id=None,
-#             name=_generate_slug(collection.title, lookup),
-#         )
-#     )
+    original_collection = (
+        db.query(Collection)
+        .filter(Collection.import_id == collection.import_id)
+        .one_or_none()
+    )
 
-#     # TODO: Validate the metadata
+    if original_collection is None:  # Not found the collection to update
+        _LOGGER.error(f"Unable to find collection for update {collection}")
+        return False
 
-#     # Add the metadata
-#     db.flush()
-#     tax = (
-#         db.query(MetadataOrganisation)
-#         .filter(MetadataOrganisation.organisation_id == org_id)
-#         .one()
-#     )
-#     db.add(
-#         CollectionMetadata(
-#             collection_import_id=new_collection.import_id,
-#             taxonomy_id=tax.taxonomy_id,
-#             value=collection.metadata,
-#         )
-#     )
-#     return collection
+    # We will need to update the slug if the title has changed
+    update_title = cast(str, original_collection.title) != collection.title
+
+    result = db.execute(
+        db_update(Collection)
+        .where(Collection.import_id == collection.import_id)
+        .values(
+            title=new_values["title"],
+            description=new_values["summary"],
+        )
+    )
+    if result.rowcount == 0:  # type: ignore
+        msg = "Could not update collection fields: {collection}"
+        _LOGGER.error(msg)
+        raise RepositoryError(msg)
+
+    # update slug if title changed
+    if update_title:
+        db.flush()
+        name = generate_slug(db, collection.title)
+        new_slug = Slug(
+            collection_import_id=collection.import_id,
+            collection_document_import_id=None,
+            name=name,
+        )
+        db.add(new_slug)
+        _LOGGER.info(f"Added a new slug for {collection.import_id} of {new_slug.name}")
+
+    return True
 
 
-# def delete(db: Session, import_id: str) -> bool:
-#     """
-#     Deletes a single collection by the import id.
+def create(
+    db: Session, collection: CollectionDTO, org_id: int
+) -> Optional[CollectionDTO]:
+    """
+    Creates a new collection.
 
-#     :param str import_id: The collection import id to delete.
-#     :return bool: True if deleted False if not.
-#     """
-#     commands = [
-#         db_delete(CollectionOrganisation).where(
-#             CollectionOrganisation.collection_import_id == import_id
-#         ),
-#         db_delete(CollectionMetadata).where(CollectionMetadata.
-# collection_import_id == import_id),
-#         db_delete(Collection).where(Collection.import_id == import_id),
-#     ]
-#     for c in commands:
-#         result = db.execute(c)
+    :param CollectionDTO collection: the values for the new collection
+    :param int org_id: a validated organisation id
+    :return Optional[CollectionDTO]: the new collection created
+    """
+    try:
+        new_collection, new_fam_org = _collection_org_from_dto(collection, org_id)
+        db.add(new_collection)
+        db.add(new_fam_org)
+    except Exception as e:
+        _LOGGER.error(e)
+        return
 
-#     return result.rowcount > 0  # type: ignore
+    # Add a slug
+    db.add(
+        Slug(
+            collection_import_id=collection.import_id,
+            collection_document_import_id=None,
+            name=generate_slug(db, collection.title),
+        )
+    )
+
+    return collection
+
+
+def delete(db: Session, import_id: str) -> bool:
+    """
+    Deletes a single collection by the import id.
+
+    :param str import_id: The collection import id to delete.
+    :return bool: True if deleted False if not.
+    """
+    command = db_delete(Collection).where(Collection.import_id == import_id)
+    result = db.execute(command)
+
+    return result.rowcount > 0  # type: ignore
---
+++ app/repository/helpers.py
@@ -0,0 +1,42 @@
+"""Helper functions for repos"""
+
+from typing import cast
+from uuid import uuid4
+from slugify import slugify
+from sqlalchemy.orm import Session
+
+from app.db.models.law_policy.family import Slug
+
+
+def generate_slug(
+    db: Session,
+    title: str,
+    attempts: int = 100,
+    suffix_length: int = 4,
+) -> str:
+    """
+    Generates a slug that doesn't already exists.
+
+    :param Session db: The connection to the db
+    :param str title: The title or name of the object you wish to slugify
+    :param int attempts: The number of attempt to generate a unique slug before
+    failing, defaults to 100
+    :param int suffix_length: The suffix to produce uniqueness, defaults to 4
+    :raises RuntimeError: If we cannot produce a unique slug
+    :return str: the slug generated
+    """
+    lookup = set([cast(str, n) for n in db.query(Slug.name).all()])
+    base = slugify(str(title))
+
+    # TODO: try to extend suffix length if attempts are exhausted
+    suffix = str(uuid4())[:suffix_length]
+    count = 0
+    while (slug := f"{base}_{suffix}") in lookup:
+        count += 1
+        suffix = str(uuid4())[:suffix_length]
+        if count > attempts:
+            raise RuntimeError(
+                f"Failed to generate a slug for {base} after {attempts} attempts."
+            )
+    lookup.add(slug)
+    return slug
