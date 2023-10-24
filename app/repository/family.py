ae935679de4237f26d6cf56750acfa8006008908
diversemix <diversemix@gmail.com>
2023-10-24 16:25:47 +0100

    Fix FIXME and code!

--- app/repository/family.py
+++ app/repository/family.py
@@ -253,12 +253,9 @@ def create(db: Session, family: FamilyCreateDTO, geo_id: int, org_id: int) -> st
     """
     try:
         new_family, new_fam_org = _family_org_from_dto(family, geo_id, org_id)
-        org_name = (
-            "CCLW"  # FIXME: https://linear.app/climate-policy-radar/issue/PDCT-494
-        )
 
         new_family.import_id = cast(
-            Column, generate_import_id(db, CountedEntity.Family, org_name)
+            Column, generate_import_id(db, CountedEntity.Family, org_id)
         )
         new_fam_org.family_import_id = new_family.import_id
 
