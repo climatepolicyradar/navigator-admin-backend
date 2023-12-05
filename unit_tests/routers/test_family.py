3d77e361065f660ba8a433a07f6f0de6a35e6e94
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2023-12-05 14:51:16 +0000

    Added logging error when timeout exception raised.

--- unit_tests/routers/test_family.py
+++ unit_tests/routers/test_family.py
@@ -60,12 +60,17 @@ def test_search_when_invalid_params(
 
 
 def test_search_when_request_timeout(
-    client: TestClient, family_service_mock, user_header_token
+    client: TestClient, family_service_mock, user_header_token, caplog
 ):
     family_service_mock.throw_timeout_error = True
-    response = client.get("/api/v1/families/?q=timeout", headers=user_header_token)
+    with caplog.at_level(logging.INFO):
+        response = client.get("/api/v1/families/?q=timeout", headers=user_header_token)
     assert response.status_code == status.HTTP_408_REQUEST_TIMEOUT
     assert family_service_mock.search.call_count == 1
+    assert (
+        "Request timed out fetching matching families. Try adjusting your query."
+        in caplog.text
+    )
 
 
 def test_search_when_not_found(
