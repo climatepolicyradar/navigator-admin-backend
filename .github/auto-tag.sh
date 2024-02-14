e1def0eb4942a2e30714357966c1de617f416d08
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-14 11:16:09 +0000

    Fix auto-incrementing using arithmetic expansion.

--- .github/auto-tag.sh
+++ .github/auto-tag.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # Argument is going to be the pull request body.
-latest_tag=$1
+latest_tag="$1"
 echo "Latest tag: ${latest_tag}"
+
+# Extract the version numbers from the tag
+version_numbers=${latest_tag#v}       # Remove the leading 'v'
+version_numbers=${version_numbers%-*} # Remove the trailing '-beta'
 
 pull_request_body="$2"
 echo "${pull_request_body}"
@@ -33,12 +37,6 @@ if { [ "${is_major}" = true ] && [ "${is_minor}" = true ]; } ||
 elif { [ "${is_major}" = false ] && [ "${is_minor}" = false ] && [ "${is_breaking_change}" = false ]; }; then
 	echo "No tag information found. Auto-tagging failed..."
 else
-	# Extract the version numbers from the tag
-	version_numbers=${latest_tag#v} # Remove the leading 'v'
-	echo "Version number without the v: ${version_numbers}"
-	version_numbers=${version_numbers%-*} # Remove the trailing '-beta'
-	echo "Version number without the beta: ${version_numbers}"
-
 	# Split the version numbers into their respective parts
 	breaking_change_version=$(echo "${version_numbers}" | cut -d'.' -f1)
 	major_version=$(echo "${version_numbers}" | cut -d'.' -f2)
@@ -46,13 +44,16 @@ else
 	# Auto-tag based on selected option.
 	if [ "${is_minor}" = true ]; then
 		minor_version=$(echo "${version_numbers}" | cut -d'.' -f3)
-		new_tag=v${breaking_change_version}.${major_version}.${minor_version+ 1}-beta
+		new_minor_version=$((minor_version + 1))
+		new_tag=v${breaking_change_version}.${major_version}.${new_minor_version}-beta
 		echo "Tagging as new minor version ${new_tag}..."
 	elif [ "${is_major}" = true ]; then
-		new_tag="v${breaking_change_version}.${major_version+ 1}.0-beta"
+		new_major_version=$((major_version + 1))
+		new_tag="v${breaking_change_version}.${new_major_version}.0-beta"
 		echo "Tagging as new major version ${new_tag}..."
 	elif [ "${is_breaking_change}" = true ]; then
-		new_tag=v${breaking_change_version+ 1}.0.0-beta
+		new_breaking_version=$((breaking_change_version + 1))
+		new_tag=v${new_breaking_version}.0.0-beta
 		echo "Tagging as new breaking change ${new_tag}..."
 	fi
 fi
