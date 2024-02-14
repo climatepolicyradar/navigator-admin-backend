7a79b5693d9a195d9de4503e0c03feef1ddd1065
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-14 11:49:33 +0000

    Updated version variable names to match semver.

--- .github/auto-tag.sh
+++ .github/auto-tag.sh
@@ -11,49 +11,49 @@ version_numbers=${version_numbers%-*} # Remove the trailing '-beta'
 pull_request_body="$2"
 echo "${pull_request_body}"
 
+# Check if patch version.
+is_patch=false
+if [ $(echo "${pull_request_body}" | grep -c "\[x\] Patch") -gt 0 ]; then
+	is_patch=true
+fi
+
 # Check if minor version.
 is_minor=false
 if [ $(echo "${pull_request_body}" | grep -c "\[x\] Minor version") -gt 0 ]; then
 	is_minor=true
 fi
 
-# Check if major version.
+# Check if major change.
 is_major=false
 if [ $(echo "${pull_request_body}" | grep -c "\[x\] Major version") -gt 0 ]; then
 	is_major=true
 fi
 
-# Check if breaking change.
-is_breaking_change=false
-if [ $(echo "${pull_request_body}" | grep -c "\[x\] Breaking change") -gt 0 ]; then
-	is_breaking_change=true
-fi
-
 # If multiple have been checked or none have been checked, don't auto tag.
-if { [ "${is_major}" = true ] && [ "${is_minor}" = true ]; } ||
-	{ [ "${is_major}" = true ] && [ "${is_breaking_change}" = true ]; } ||
-	{ [ "${is_minor}" = true ] && [ "${is_breaking_change}" = true ]; }; then
+if { [ "${is_minor}" = true ] && [ "${is_patch}" = true ]; } ||
+	{ [ "${is_minor}" = true ] && [ "${is_major}" = true ]; } ||
+	{ [ "${is_patch}" = true ] && [ "${is_major}" = true ]; }; then
 	echo "Ambiguous tag information. Auto-tagging failed..."
-elif { [ "${is_major}" = false ] && [ "${is_minor}" = false ] && [ "${is_breaking_change}" = false ]; }; then
+elif { [ "${is_minor}" = false ] && [ "${is_patch}" = false ] && [ "${is_major}" = false ]; }; then
 	echo "No tag information found. Auto-tagging failed..."
 else
 	# Split the version numbers into their respective parts
-	breaking_change_version=$(echo "${version_numbers}" | cut -d'.' -f1)
-	major_version=$(echo "${version_numbers}" | cut -d'.' -f2)
+	major_change_version=$(echo "${version_numbers}" | cut -d'.' -f1)
+	minor_version=$(echo "${version_numbers}" | cut -d'.' -f2)
 
 	# Auto-tag based on selected option.
-	if [ "${is_minor}" = true ]; then
-		minor_version=$(echo "${version_numbers}" | cut -d'.' -f3)
+	if [ "${is_patch}" = true ]; then
+		patch_version=$(echo "${version_numbers}" | cut -d'.' -f3)
+		new_patch_version=$((patch_version + 1))
+		new_tag=v${major_change_version}.${minor_version}.${new_patch_version}-beta
+		echo "Tagging as new patch ${new_tag}..."
+	elif [ "${is_minor}" = true ]; then
 		new_minor_version=$((minor_version + 1))
-		new_tag=v${breaking_change_version}.${major_version}.${new_minor_version}-beta
+		new_tag="v${major_change_version}.${new_minor_version}.0-beta"
 		echo "Tagging as new minor version ${new_tag}..."
 	elif [ "${is_major}" = true ]; then
-		new_major_version=$((major_version + 1))
-		new_tag="v${breaking_change_version}.${new_major_version}.0-beta"
+		new_major_version=$((major_change_version + 1))
+		new_tag=v${new_major_version}.0.0-beta
 		echo "Tagging as new major version ${new_tag}..."
-	elif [ "${is_breaking_change}" = true ]; then
-		new_breaking_version=$((breaking_change_version + 1))
-		new_tag=v${new_breaking_version}.0.0-beta
-		echo "Tagging as new breaking change ${new_tag}..."
 	fi
 fi
