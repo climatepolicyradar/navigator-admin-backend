24cbce76508bb818fdf0d0204a14026574a88ca4
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-14 10:44:00 +0000

    Increment version number based on ticked checkbox.

--- .github/auto-tag.sh
+++ .github/auto-tag.sh
@@ -2,7 +2,7 @@
 
 # Argument is going to be the pull request body.
 latest_tag=$1
-echo "${latest_tag}"
+echo "Latest tag: ${latest_tag}"
 
 pull_request_body="$2"
 echo "${pull_request_body}"
@@ -33,12 +33,26 @@ if { [ "${is_major}" = true ] && [ "${is_minor}" = true ]; } ||
 elif { [ "${is_major}" = false ] && [ "${is_minor}" = false ] && [ "${is_breaking_change}" = false ]; }; then
 	echo "No tag information found. Auto-tagging failed..."
 else
+	# Extract the version numbers from the tag
+	version_numbers=${latest_tag#v} # Remove the leading 'v'
+	echo "Version number without the v: ${version_numbers}"
+	version_numbers=${version_numbers%-*} # Remove the trailing '-beta'
+	echo "Version number without the beta: ${version_numbers}"
+
+	# Split the version numbers into their respective parts
+	breaking_change_version=$(echo "${version_numbers}" | cut -d'.' -f1)
+	major_version=$(echo "${version_numbers}" | cut -d'.' -f2)
+
 	# Auto-tag based on selected option.
 	if [ "${is_minor}" = true ]; then
-		echo "Tagging as new minor version..."
+		minor_version=$(echo "${version_numbers}" | cut -d'.' -f3)
+		new_tag=v${breaking_change_version}.${major_version}.${minor_version+ 1}-beta
+		echo "Tagging as new minor version ${new_tag}..."
 	elif [ "${is_major}" = true ]; then
-		echo "Tagging as new major version..."
+		new_tag="v${breaking_change_version}.${major_version+ 1}.0-beta"
+		echo "Tagging as new major version ${new_tag}..."
 	elif [ "${is_breaking_change}" = true ]; then
-		echo "Tagging as new breaking change..."
+		new_tag=v${breaking_change_version+ 1}.0.0-beta
+		echo "Tagging as new breaking change ${new_tag}..."
 	fi
 fi
