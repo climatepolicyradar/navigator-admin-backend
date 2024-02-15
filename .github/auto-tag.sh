9bfca9dbcb45bb5a7ca7c6c02a08111ba7c8169f
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-15 10:16:43 +0000

    Updated conditionals to use int instead of bool.

--- .github/auto-tag.sh
+++ .github/auto-tag.sh
@@ -28,12 +28,12 @@ is_major=$(is_major_selected "${pr_body}")
 
 # If multiple have been checked or none have been checked, don't auto tag.
 pr_number="$2"
-if { [ "${is_minor}" = true ] && [ "${is_patch}" = true ]; } ||
-	{ [ "${is_minor}" = true ] && [ "${is_major}" = true ]; } ||
-	{ [ "${is_patch}" = true ] && [ "${is_major}" = true ]; }; then
+if { [[ "${is_minor}" ]] && [[ "${is_patch}" ]]; } ||
+	{ [[ "${is_minor}" ]] && [[ "${is_major}" ]]; } ||
+	{ [[ "${is_patch}" ]] && [[ "${is_major}" ]]; }; then
 	echo "Ambiguous tag information in body of pull request #${pr_number}. Auto-tagging failed..."
 	exit 1
-elif { [ "${is_minor}" = false ] && [ "${is_patch}" = false ] && [ "${is_major}" = false ]; }; then
+elif { [[ ! ${is_minor} == 0 ]] && [[ ! ${is_patch} == 0 ]] && [[ ! ${is_major} == 0 ]]; }; then
 	echo "No tag information found in body of pull request #${pr_number}. Auto-tagging failed..."
 	exit 1
 else
@@ -42,16 +42,16 @@ else
 	minor_version=$(get_minor "${version_numbers}")
 
 	# Auto-tag based on selected option.
-	if [ "${is_patch}" = true ]; then
+	if [[ "${is_patch}" ]]; then
 		patch_version=$(get_patch "${version_numbers}")
 		new_patch_version=$(increment "${patch_version}")
 		new_tag=v${major_version}.${minor_version}.${new_patch_version}-beta
 		echo "Tagging as new patch ${new_tag}..."
-	elif [ "${is_minor}" = true ]; then
+	elif [[ "${is_minor}" ]]; then
 		new_minor_version=$(increment "${minor_version}")
 		new_tag="v${major_version}.${new_minor_version}.0-beta"
 		echo "Tagging as new minor version ${new_tag}..."
-	elif [ "${is_major}" = true ]; then
+	elif [[ "${is_major}" ]]; then
 		new_major_version=$(increment "${major_version}")
 		new_tag=v${new_major_version}.0.0-beta
 		echo "Tagging as new major version ${new_tag}..."
