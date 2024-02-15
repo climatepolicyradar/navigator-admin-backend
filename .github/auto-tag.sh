e530432bcb2770f2966c5d9cacb1ef7b4f30af8d
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-15 12:06:11 +0000

    Determined maturity of new tag based on old tag.

--- .github/auto-tag.sh
+++ .github/auto-tag.sh
@@ -2,7 +2,7 @@
 set -e
 
 script_folder=$(dirname "${BASH_SOURCE[0]}")
-source $script_folder/funcs.sh
+source "${script_folder}"/funcs.sh
 
 # Get the latest Git tag.
 git fetch --prune --unshallow --tags # This is needed - without it no tags are found.
@@ -12,12 +12,6 @@ if [ -z "${latest_tag}" ]; then
 	exit 1
 fi
 echo "Latest tag: ${latest_tag}"
-
-# Extract the version numbers from the tag
-maturity=$(get_maturity "${latest_tag}")
-echo "Maturity: ${latest_tag}"
-version_numbers=${latest_tag#v}       # Remove the leading 'v'
-version_numbers=${version_numbers%-*} # Remove the trailing '-beta'
 
 pr_body="$1"
 
@@ -34,21 +28,29 @@ if { [[ ${is_minor} == true ]] && [[ ${is_patch} == true ]]; } ||
 	echo "Ambiguous tag information in body of pull request #${pr_number}. Auto-tagging will use most senior version option selected."
 fi
 
-# Auto-tag based on most senior version selected.
+# Extract the version numbers from the tag
 major_version=$(get_major "${version_numbers}")
 minor_version=$(get_minor "${version_numbers}")
 patch_version=$(get_patch "${version_numbers}")
+maturity=$(get_maturity "${latest_tag}")
+if [ ! -z "${maturity}" ]; then
+	maturity="-${maturity}"
+fi
+version_numbers=${latest_tag#v}       # Remove the leading 'v'
+version_numbers=${version_numbers%-*} # Remove the trailing '-beta'
+
+# Auto-tag based on most senior version selected.
 if [[ ${is_major} == true ]]; then
 	new_major_version=$(increment "${major_version}")
-	new_tag=v${new_major_version}.0.0-beta
+	new_tag=v${new_major_version}.0.0${maturity}
 	echo "Tagging as new major version ${new_tag}..."
 elif [[ ${is_minor} == true ]]; then
 	new_minor_version=$(increment "${minor_version}")
-	new_tag="v${major_version}.${new_minor_version}.0-beta"
+	new_tag="v${major_version}.${new_minor_version}.0${maturity}"
 	echo "Tagging as new minor version ${new_tag}..."
 else
 	new_patch_version=$(increment "${patch_version}")
-	new_tag=v${major_version}.${minor_version}.${new_patch_version}-beta
+	new_tag=v${major_version}.${minor_version}.${new_patch_version}${maturity}
 	echo "Tagging as new patch ${new_tag}..."
 fi
 
