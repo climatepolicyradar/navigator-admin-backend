a2f39f59f2a6b0848264377300397a509c0c0c77
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-20 11:44:29 +0000

    Removed merge flag from Git tag filter.

--- .github/funcs.sh
+++ .github/funcs.sh
@@ -108,6 +108,6 @@ is_major_selected() {
 
 get_latest_tag() {
 	git fetch --prune --unshallow --tags # This is needed - without it no tags are found.
-	latest_tag=$(git tag --list 'v*' --sort=-creatordate --merged | head -n1)
+	latest_tag=$(git tag --list 'v*' --sort=-creatordate | head -n1)
 	echo "${latest_tag}"
 }
