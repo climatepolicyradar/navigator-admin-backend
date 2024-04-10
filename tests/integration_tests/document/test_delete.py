9a73f3d315f4ce4739fde7cb32124e2243748db1
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-04-10 18:27:18 +0100

    Added integration test to check 400 returned on doc delete if org mismatch

--- tests/integration_tests/document/test_delete.py
+++ tests/integration_tests/document/test_delete.py
@@ -93,3 +93,18 @@ def test_delete_document_when_db_error(
     data = response.json()
     assert data["detail"] == "Bad Repo"
     assert bad_document_repo.delete.call_count == 1
+
+
+def test_delete_document_when_org_mismatch(
+    client: TestClient, data_db: Session, non_cclw_user_header_token
+):
+    setup_db(data_db)
+    response = client.delete(
+        "/api/v1/documents/D.0.0.1", headers=non_cclw_user_header_token
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    data = response.json()
+    assert (
+        data["detail"]
+        == "Current user does not belong to the organisation that owns document D.0.0.1"
+    )
