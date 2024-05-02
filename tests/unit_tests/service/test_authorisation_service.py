acf2ddbf5ce6b077c390b1742304c7e5bce9a671
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-05-02 17:14:06 +0100

    Move all mocks under new /tests/mocks dir

--- tests/unit_tests/service/test_authorisation_service.py
+++ tests/unit_tests/service/test_authorisation_service.py
@@ -3,7 +3,7 @@ import pytest
 import app.service.authentication as auth_service
 import app.service.token as token_service
 from app.errors import AuthenticationError, RepositoryError
-from tests.unit_tests.mocks.repos.app_user_repo import (
+from tests.mocks.repos.app_user_repo import (
     HASH_PASSWORD,
     PLAIN_PASSWORD,
     VALID_USERNAME,
