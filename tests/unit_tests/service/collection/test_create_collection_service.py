bb384d249bbed91d96b0e0c3c157943c857c3bca
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-05-07 10:29:47 +0100

    Use existing helper functions in collection service unit tests

--- tests/unit_tests/service/collection/test_create_collection_service.py
+++ tests/unit_tests/service/collection/test_create_collection_service.py
@@ -2,41 +2,26 @@ import pytest
 
 import app.service.collection as collection_service
 from app.errors import RepositoryError
-from app.model.collection import CollectionCreateDTO, CollectionReadDTO
-from tests.mocks.repos.collection_repo import create_collection_read_dto as create_dto
-
-
-def _to_create_dto(dto: CollectionReadDTO) -> CollectionCreateDTO:
-    return CollectionCreateDTO(
-        title=dto.title,
-        description=dto.description,
-    )
-
+from tests.helpers.collection import create_collection_create_dto
 
 # --- CREATE
 
 USER_EMAIL = "test@cpr.org"
 
 
-def test_create(
-    collection_repo_mock,
-    app_user_repo_mock,
-):
-    new_collection = create_dto(import_id="A.0.0.5")
-    collection = collection_service.create(_to_create_dto(new_collection), USER_EMAIL)
+def test_create(collection_repo_mock, app_user_repo_mock):
+    new_collection = create_collection_create_dto()
+    collection = collection_service.create(new_collection, USER_EMAIL)
     assert collection is not None
     assert collection_repo_mock.create.call_count == 1
     assert app_user_repo_mock.get_org_id.call_count == 1
 
 
-def test_create_when_db_fails(
-    collection_repo_mock,
-    app_user_repo_mock,
-):
-    new_collection = create_dto(import_id="a.b.c.d")
+def test_create_when_db_fails(collection_repo_mock, app_user_repo_mock):
+    new_collection = create_collection_create_dto()
     collection_repo_mock.return_empty = True
     with pytest.raises(RepositoryError) as e:
-        collection_service.create(_to_create_dto(new_collection), USER_EMAIL)
+        collection_service.create(new_collection, USER_EMAIL)
     assert e.value.message == "Error when creating collection 'description'"
     assert collection_repo_mock.create.call_count == 1
     assert app_user_repo_mock.get_org_id.call_count == 1
--- tests/unit_tests/service/collection/test_update_collection_service.py
+++ tests/unit_tests/service/collection/test_update_collection_service.py
@@ -2,35 +2,22 @@ import pytest
 
 import app.service.collection as collection_service
 from app.errors import RepositoryError, ValidationError
-from app.model.collection import CollectionReadDTO, CollectionWriteDTO
 from tests.helpers.collection import create_collection_write_dto
-
-
-def _to_write_dto(dto: CollectionReadDTO) -> CollectionWriteDTO:
-    return CollectionWriteDTO(
-        title=dto.title,
-        description=dto.description,
-        organisation=dto.organisation,
-    )
-
 
 # --- UPDATE
 
 
-def test_update(
-    collection_repo_mock,
-):
+def test_update(collection_repo_mock):
     collection = collection_service.get("a.b.c.d")
     assert collection is not None
 
-    result = collection_service.update("a.b.c.d", _to_write_dto(collection))
+    updated_collection = create_collection_write_dto()
+    result = collection_service.update("a.b.c.d", updated_collection)
     assert result is not None
     assert collection_repo_mock.update.call_count == 1
 
 
-def test_update_when_missing(
-    collection_repo_mock,
-):
+def test_update_when_missing(collection_repo_mock):
     collection = create_collection_write_dto()
     assert collection is not None
     collection_repo_mock.return_empty = True
@@ -43,9 +30,7 @@ def test_update_when_missing(
     assert e.value.message == expected_msg
 
 
-def test_update_raises_when_invalid_id(
-    collection_repo_mock,
-):
+def test_update_raises_when_invalid_id(collection_repo_mock):
     collection = create_collection_write_dto()
 
     with pytest.raises(ValidationError) as e:
