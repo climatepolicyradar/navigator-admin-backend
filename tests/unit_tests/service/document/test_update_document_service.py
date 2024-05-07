db32418d04c1f78e12eae9905013ce7c89602022
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-05-07 10:39:08 +0100

    Use existing helper functions in document service unit tests

--- tests/unit_tests/service/document/test_update_document_service.py
+++ tests/unit_tests/service/document/test_update_document_service.py
@@ -2,56 +2,41 @@ import pytest
 
 import app.service.document as doc_service
 from app.errors import RepositoryError, ValidationError
-from app.model.document import DocumentReadDTO, DocumentWriteDTO
-
-
-def _to_write_dto(dto: DocumentReadDTO) -> DocumentWriteDTO:
-    return DocumentWriteDTO(
-        variant_name=dto.variant_name,
-        role=dto.role,
-        type=dto.type,
-        title=dto.title,
-        source_url=dto.source_url,
-        user_language_name=dto.user_language_name,
-    )
-
+from tests.helpers.document import create_document_write_dto
 
 # --- UPDATE
 
 
-def test_update(
-    document_repo_mock,
-):
+def test_update(document_repo_mock):
     document = doc_service.get("a.b.c.d")
     assert document is not None
 
-    result = doc_service.update(document.import_id, _to_write_dto(document))
+    updated_doc = create_document_write_dto()
+    result = doc_service.update(document.import_id, updated_doc)
     assert result is not None
     assert document_repo_mock.update.call_count == 1
 
 
-def test_update_when_missing(
-    document_repo_mock,
-):
+def test_update_when_missing(document_repo_mock):
     document = doc_service.get("a.b.c.d")
     assert document is not None
     document_repo_mock.return_empty = True
 
+    updated_doc = create_document_write_dto()
     with pytest.raises(RepositoryError) as e:
-        doc_service.update(document.import_id, _to_write_dto(document))
+        doc_service.update(document.import_id, updated_doc)
     assert e.value.message == "Error when updating document a.b.c.d"
     assert document_repo_mock.update.call_count == 1
 
 
-def test_update_raises_when_invalid_id(
-    document_repo_mock,
-):
+def test_update_raises_when_invalid_id(document_repo_mock):
     document = doc_service.get("a.b.c.d")
     assert document is not None  # needed to placate pyright
     document.import_id = "invalid"
 
+    updated_doc = create_document_write_dto()
     with pytest.raises(ValidationError) as e:
-        doc_service.update(document.import_id, _to_write_dto(document))
+        doc_service.update(document.import_id, updated_doc)
     expected_msg = f"The import id {document.import_id} is invalid!"
     assert e.value.message == expected_msg
     assert document_repo_mock.update.call_count == 0
@@ -62,8 +47,9 @@ def test_update_raises_when_invalid_variant(document_repo_mock):
     assert document is not None  # needed to placate pyright
     document.variant_name = ""
 
+    updated_doc = create_document_write_dto(variant_name="")
     with pytest.raises(ValidationError) as e:
-        doc_service.update(document.import_id, _to_write_dto(document))
+        doc_service.update(document.import_id, updated_doc)
     expected_msg = "Variant name is empty"
     assert e.value.message == expected_msg
     assert document_repo_mock.update.call_count == 0
