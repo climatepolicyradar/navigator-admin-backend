7ede131623803b16cc4eb1887ad5b1730bbe8c91
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-05-23 13:20:08 +0100

    Update service count unit tests to only count entities in user org

--- tests/unit_tests/service/document/test_count_document_service.py
+++ tests/unit_tests/service/document/test_count_document_service.py
@@ -3,27 +3,34 @@ import pytest
 import app.service.document as doc_service
 from app.errors import RepositoryError
 
+USER_EMAIL = "test@cpr.org"
 # --- COUNT
 
 
-def test_count(document_repo_mock):
-    result = doc_service.count()
+def test_count(document_repo_mock, app_user_repo_mock):
+    result = doc_service.count(USER_EMAIL)
     assert result is not None
+    assert app_user_repo_mock.get_org_id.call_count == 1
+    assert app_user_repo_mock.is_superuser.call_count == 1
     assert document_repo_mock.count.call_count == 1
 
 
-def test_count_returns_none(document_repo_mock):
+def test_count_returns_none(document_repo_mock, app_user_repo_mock):
     document_repo_mock.return_empty = True
-    result = doc_service.count()
+    result = doc_service.count(USER_EMAIL)
     assert result is None
+    assert app_user_repo_mock.get_org_id.call_count == 1
+    assert app_user_repo_mock.is_superuser.call_count == 1
     assert document_repo_mock.count.call_count == 1
 
 
-def test_count_raises_if_db_error(document_repo_mock):
+def test_count_raises_if_db_error(document_repo_mock, app_user_repo_mock):
     document_repo_mock.throw_repository_error = True
     with pytest.raises(RepositoryError) as e:
-        doc_service.count()
+        doc_service.count(USER_EMAIL)
 
-    expected_msg = "bad repo"
+    expected_msg = "bad document repo"
     assert e.value.message == expected_msg
+    assert app_user_repo_mock.get_org_id.call_count == 1
+    assert app_user_repo_mock.is_superuser.call_count == 1
     assert document_repo_mock.count.call_count == 1
--- tests/unit_tests/service/family/test_count_family_service.py
+++ tests/unit_tests/service/family/test_count_family_service.py
@@ -9,30 +9,35 @@ import pytest
 import app.service.family as family_service
 from app.errors import RepositoryError
 
-USER_EMAIL = "cclw@cpr.org"
-ORG_ID = 1
+USER_EMAIL = "test@cpr.org"
 
 # --- COUNT
 
 
-def test_count(family_repo_mock):
-    result = family_service.count()
+def test_count(family_repo_mock, app_user_repo_mock):
+    result = family_service.count(USER_EMAIL)
     assert result is not None
+    assert app_user_repo_mock.get_org_id.call_count == 1
+    assert app_user_repo_mock.is_superuser.call_count == 1
     assert family_repo_mock.count.call_count == 1
 
 
-def test_count_returns_none(family_repo_mock):
+def test_count_returns_none(family_repo_mock, app_user_repo_mock):
     family_repo_mock.return_empty = True
-    result = family_service.count()
+    result = family_service.count(USER_EMAIL)
     assert result is None
+    assert app_user_repo_mock.get_org_id.call_count == 1
+    assert app_user_repo_mock.is_superuser.call_count == 1
     assert family_repo_mock.count.call_count == 1
 
 
-def test_count_raises_if_db_error(family_repo_mock):
+def test_count_raises_if_db_error(family_repo_mock, app_user_repo_mock):
     family_repo_mock.throw_repository_error = True
     with pytest.raises(RepositoryError) as e:
-        family_service.count()
+        family_service.count(USER_EMAIL)
 
-    expected_msg = "bad repo"
+    expected_msg = "bad family repo"
     assert e.value.message == expected_msg
+    assert app_user_repo_mock.get_org_id.call_count == 1
+    assert app_user_repo_mock.is_superuser.call_count == 1
     assert family_repo_mock.count.call_count == 1
