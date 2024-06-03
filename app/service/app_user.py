194983fa8ed1211fd0956b9a8c4916bc773ab582
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-06-03 15:42:10 +0100

    Move is_authorised_to_make_changes to app_user

--- app/service/app_user.py
+++ app/service/app_user.py
@@ -1,9 +1,13 @@
+import logging
 from typing import Optional
 
 from sqlalchemy.orm import Session
 
-from app.errors import ValidationError
+from app.errors import AuthorisationError, ValidationError
 from app.repository import app_user_repo
+from app.service import organisation
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def get_organisation(db: Session, user_email: str) -> int:
@@ -23,5 +27,24 @@ def restrict_entities_to_user_org(db: Session, user_email: str) -> Optional[int]
     org_id = get_organisation(db, user_email)
     superuser: bool = is_superuser(db, user_email)
     if superuser:
+        _LOGGER.error("IS SUPERUSER")
         return None
+    _LOGGER.error("ORG ID: %s", org_id)
     return org_id
+
+
+def is_authorised_to_make_changes(
+    db, user_email: str, org_name: str, entity: str, import_id: str
+) -> bool:
+    """Validate entity belongs to same org as current user."""
+
+    user_org_id = restrict_entities_to_user_org(db, user_email)
+    if user_org_id is None:
+        return True
+
+    entity_org_id = organisation.get_id(db, org_name)
+    if entity_org_id != user_org_id:
+        msg = f"User '{user_email}' is not authorised to make changes to {entity} '{import_id}'"
+        raise AuthorisationError(msg)
+
+    return bool(entity_org_id == user_org_id)
