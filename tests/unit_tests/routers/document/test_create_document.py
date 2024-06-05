9d1b2be16875016e33fba7bb3d95b101ce34e301
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-06-05 14:14:07 +0100

    Add unit router test coverage for validating user org on document create

--- tests/unit_tests/routers/document/test_create_document.py
+++ tests/unit_tests/routers/document/test_create_document.py
@@ -29,3 +29,31 @@ def test_create_when_family_not_found(
     data = response.json()
     assert data["detail"] == "Could not find family for this_family"
     assert document_service_mock.create.call_count == 1
+
+
+def test_create_fails_when_validation_error(
+    client: TestClient, document_service_mock, user_header_token
+):
+    document_service_mock.throw_validation_error = True
+    new_data = create_document_create_dto("validation_error").model_dump(mode="json")
+    response = client.post(
+        "/api/v1/documents", json=new_data, headers=user_header_token
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    data = response.json()
+    assert data["detail"] == "Validation error"
+    assert document_service_mock.delete.call_count == 0
+
+
+def test_create_fails_when_org_mismatch(
+    client: TestClient, document_service_mock, user_header_token
+):
+    document_service_mock.org_mismatch = True
+    new_data = create_document_create_dto("unfccc_fam").model_dump(mode="json")
+    response = client.post(
+        "/api/v1/documents", json=new_data, headers=user_header_token
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    data = response.json()
+    assert data["detail"] == "Org mismatch"
+    assert document_service_mock.delete.call_count == 0
