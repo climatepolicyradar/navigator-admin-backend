a75b0b902d5dbd0fa25e5fd16756f509f47701dd
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-06-05 16:53:20 +0100

    Rename attribute as 'superuser'

--- tests/mocks/services/app_user_service.py
+++ tests/mocks/services/app_user_service.py
@@ -7,7 +7,7 @@ ORG_ID = 1234
 
 def mock_app_user_service(app_user_service, monkeypatch: MonkeyPatch, mocker):
     app_user_service.invalid_org = False
-    app_user_service.is_superuser = False
+    app_user_service.superuser = False
 
     def mock_get_organisation(_, user_email: str) -> int:
         if app_user_service.invalid_org is True:
@@ -15,7 +15,7 @@ def mock_app_user_service(app_user_service, monkeypatch: MonkeyPatch, mocker):
         return 1
 
     def mock_restrict_entities_to_user_org(_, user_email: str) -> Optional[int]:
-        if app_user_service.is_superuser is True:
+        if app_user_service.superuser is True:
             return None
         return 1
 
