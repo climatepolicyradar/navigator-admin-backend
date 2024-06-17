3647b9810baa60d3bac852bd63a5cf65f94d10e4
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-06-17 10:02:28 +0100

    Correctly export router symbols using __all__

--- app/api/api_v1/routers/__init__.py
+++ app/api/api_v1/routers/__init__.py
@@ -1,7 +1,17 @@
-from .analytics import analytics_router
-from .auth import auth_router
-from .collection import collections_router
-from .config import config_router
-from .document import document_router
-from .event import event_router
-from .family import families_router
+from app.api.api_v1.routers.analytics import analytics_router
+from app.api.api_v1.routers.auth import auth_router
+from app.api.api_v1.routers.collection import collections_router
+from app.api.api_v1.routers.config import config_router
+from app.api.api_v1.routers.document import document_router
+from app.api.api_v1.routers.event import event_router
+from app.api.api_v1.routers.family import families_router
+
+__all__ = (
+    "analytics_router",
+    "auth_router",
+    "collections_router",
+    "config_router",
+    "document_router",
+    "event_router",
+    "families_router",
+)
