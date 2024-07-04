f47b2da13794bad360f3e367edb61e2e8d51db86
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-07-04 22:47:57 +0100

    Remove rogue debug

--- app/repository/collection.py
+++ app/repository/collection.py
@@ -277,7 +277,6 @@ def count(db: Session, org_id: Optional[int]) -> Optional[int]:
         if org_id is not None:
             query = query.filter(Organisation.id == org_id)
         n_collections = query.count()
-        _LOGGER.error(n_collections)
     except Exception as e:
         _LOGGER.error(e)
         return
