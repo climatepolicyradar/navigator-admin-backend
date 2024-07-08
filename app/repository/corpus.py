728876175c642f6ab5329f38fa19cd9f70456557
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-07-08 15:23:15 +0100

    Move get_taxonomy_from_corpus to db_client

--- app/repository/corpus.py
+++ app/repository/corpus.py
@@ -1,11 +1,8 @@
 import logging
 from typing import Optional
 
-from db_client.models.organisation import Corpus, CorpusType
+from db_client.models.organisation import Corpus
 from sqlalchemy.orm import Session
-
-from app.errors import ValidationError
-from app.model.config import TaxonomyData
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,28 +28,3 @@ def validate(db: Session, corpus_id: str) -> bool:
     """
     corpora = [corpus[0] for corpus in db.query(Corpus.import_id).distinct().all()]
     return bool(corpus_id in corpora)
-
-
-def get_taxonomy_from_corpus(
-    db: Session, corpus_id: str, _entity_key: Optional[str] = None
-) -> Optional[TaxonomyData]:
-    """Get the taxonomy of a corpus.
-
-    :param Session db: The DB session to connect to.
-    :param str corpus_id: The corpus import ID we want to get the taxonomy
-        for.
-    :return Optional[str]: Return the taxonomy of the given corpus or None.
-    """
-    tax = (
-        db.query(CorpusType.valid_metadata)
-        .join(Corpus, Corpus.corpus_type_name == CorpusType.name)
-        .filter(Corpus.import_id == corpus_id)
-        .scalar()
-    )
-    if tax is not None and _entity_key is not None:
-        if _entity_key not in tax.keys():
-            raise ValidationError(
-                f"Cannot find {_entity_key} taxonomy data in database"
-            )
-        tax = tax[_entity_key]
-    return tax
--- tests/mocks/repos/corpus_repo.py
+++ tests/mocks/repos/corpus_repo.py
@@ -1,14 +1,11 @@
-from typing import Optional, cast
+from typing import Optional
 
 from pytest import MonkeyPatch
-
-from app.model.config import TaxonomyData
 
 
 def mock_corpus_repo(corpus_repo, monkeypatch: MonkeyPatch, mocker):
     corpus_repo.error = False
     corpus_repo.valid = True
-    corpus_repo.bad_taxonomy = False
 
     def mock_get_corpus_org_id(_, __) -> Optional[int]:
         if not corpus_repo.error:
@@ -17,25 +14,8 @@ def mock_corpus_repo(corpus_repo, monkeypatch: MonkeyPatch, mocker):
     def mock_validate(_, __) -> bool:
         return corpus_repo.valid
 
-    def mock_get_taxonomy_from_corpus(_, __, ___=None) -> Optional[TaxonomyData]:
-        if corpus_repo.bad_taxonomy:
-            return None
-
-        color = {
-            "allow_blanks": False,
-            "allow_any": False,
-            "allowed_values": ["pink", "blue"],
-        }
-        size = {"allow_blanks": True, "allow_any": True, "allowed_values": []}
-        return cast(TaxonomyData, {"color": color, "size": size})
-
     monkeypatch.setattr(corpus_repo, "get_corpus_org_id", mock_get_corpus_org_id)
     mocker.spy(corpus_repo, "get_corpus_org_id")
 
     monkeypatch.setattr(corpus_repo, "validate", mock_validate)
     mocker.spy(corpus_repo, "validate")
-
-    monkeypatch.setattr(
-        corpus_repo, "get_taxonomy_from_corpus", mock_get_taxonomy_from_corpus
-    )
-    mocker.spy(corpus_repo, "get_taxonomy_from_corpus")
