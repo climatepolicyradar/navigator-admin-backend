8c935ff85e2919999f4763566408c60b5bca8f33
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-07-11 13:17:25 +0100

    Update document helpers

--- tests/helpers/document.py
+++ tests/helpers/document.py
@@ -19,7 +19,7 @@ def create_document_create_dto(
     if source_url is not None:
         source_url = cast(AnyHttpUrl, source_url)
     if metadata is None:
-        metadata = {"role": ["MAIN"]}
+        metadata = {"role": ["MAIN"], "type": ["Law"]}
     return DocumentCreateDTO(
         family_import_id=family_import_id,
         variant_name=variant_name,
@@ -35,16 +35,23 @@ def create_document_write_dto(
     title: str = "title",
     variant_name: Optional[str] = "Original Language",
     metadata: Optional[Json] = None,
+    source_url: Optional[str] = "http://update_source",
+    user_language_name: Optional[str] = "English",
 ) -> DocumentWriteDTO:
+
     if metadata is None:
-        metadata = {"role": ["MAIN"]}
+        metadata = {"role": ["MAIN"], "type": ["Law"]}
     return DocumentWriteDTO(
         variant_name=variant_name,
         type="Law",
         metadata=metadata,
         title=title,
-        source_url=cast(AnyHttpUrl, "http://source"),
-        user_language_name="Ghotuo",
+        source_url=(
+            cast(AnyHttpUrl, "http://update_source")
+            if isinstance(source_url, str)
+            else None
+        ),
+        user_language_name=user_language_name,
     )
 
 
@@ -56,7 +63,7 @@ def create_document_read_dto(
     metadata: Optional[Json] = None,
 ) -> DocumentReadDTO:
     if metadata is None:
-        metadata = {"role": ["MAIN"]}
+        metadata = {"role": ["MAIN"], "type": ["Law"]}
     return DocumentReadDTO(
         import_id=import_id,
         family_import_id=family_import_id,
