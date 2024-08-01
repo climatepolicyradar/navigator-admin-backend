3f59b1556b5024214fb4bd3e1ffc63422c08bcc9
Anna Pokorska <anna@climatepolicyradar.org>
2024-08-01 16:26:28 +0100

    Tidy up endpoint

--- app/api/api_v1/routers/ingest.py
+++ app/api/api_v1/routers/ingest.py
@@ -1,12 +1,22 @@
+import logging
+
 from fastapi import APIRouter, Request, status
 
 from app.model.ingest import IngestDTO
 
 ingest_router = r = APIRouter()
 
+_LOGGER = logging.getLogger(__name__)
+
 
 @r.post("/ingest", response_model=str, status_code=status.HTTP_201_CREATED)
-async def ingest_data(request: Request, new_data: IngestDTO) -> str:
+async def ingest_data(new_data: IngestDTO) -> str:
+    """
+    Bulk import endpoint.
 
-    print(new_data)
-    return "Hello world"
+    :param IngestDTO new_data: json representation of data to ingest.
+    :return str: corpus_id of the data to ingest into.
+    """
+
+    _LOGGER.info(new_data)
+    return new_data.corpus_id
