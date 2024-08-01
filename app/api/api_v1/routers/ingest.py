d16c746faba1137d845a32821c72ba969f7de9b6
Anna Pokorska <anna@climatepolicyradar.org>
2024-08-01 16:13:41 +0100

    Add an ingest model

--- app/api/api_v1/routers/ingest.py
+++ app/api/api_v1/routers/ingest.py
@@ -1,12 +1,29 @@
-from typing import Any
-
 from fastapi import APIRouter, Request, status
+from pydantic import BaseModel
 
 ingest_router = r = APIRouter()
 
 
+class DocumentIngestDTO(BaseModel):
+    metadata: list[str]
+    events: list[str]
+
+
+class FamilyIngestDTO(BaseModel):
+    name: str
+    summary: str
+    metadata: list[str]
+    events: list[str]
+    documents: list[DocumentIngestDTO]
+
+
+class IngestDTO(BaseModel):
+    corpus_id: str
+    families: list[FamilyIngestDTO]
+
+
 @r.post("/ingest", response_model=str, status_code=status.HTTP_201_CREATED)
-async def ingest_data(request: Request, new_data: dict[str, Any]) -> str:
+async def ingest_data(request: Request, new_data: IngestDTO) -> str:
 
     print(new_data)
     return "Hello world"
--- tests/unit_tests/routers/ingest/test_ingest.py
+++ tests/unit_tests/routers/ingest/test_ingest.py
@@ -7,6 +7,8 @@ This uses service mocks and ensures the endpoint calls into each service.
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from tests.helpers.family import create_family_create_dto
+
 
 def test_ingest_when_not_authenticated(client: TestClient):
     response = client.post(
@@ -15,9 +17,17 @@ def test_ingest_when_not_authenticated(client: TestClient):
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_create_when_ok(client: TestClient, user_header_token):
+def test_ingest_when_ok(client: TestClient, user_header_token):
+    new_family_data = create_family_create_dto("fam1").model_dump(mode="json")
+    new_family = {
+        "name": new_family_data["title"],
+        "summary": new_family_data["summary"],
+        "metadata": [],
+        "events": [],
+        "documents": [],
+    }
 
-    response = client.post(
-        "/api/v1/ingest", json={"test": "data"}, headers=user_header_token
-    )
+    new_data = {"corpus_id": "test", "families": [new_family]}
+
+    response = client.post("/api/v1/ingest", json=new_data, headers=user_header_token)
     assert response.status_code == status.HTTP_201_CREATED
