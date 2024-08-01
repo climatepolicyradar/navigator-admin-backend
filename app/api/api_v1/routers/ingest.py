256e520981c1983d2c31d14243481878a97337d8
Anna Pokorska <anna@climatepolicyradar.org>
2024-08-01 16:00:27 +0100

    Update ingest endpoint to accept json

--- app/api/api_v1/routers/ingest.py
+++ app/api/api_v1/routers/ingest.py
@@ -1,9 +1,12 @@
+from typing import Any
+
 from fastapi import APIRouter, Request, status
 
 ingest_router = r = APIRouter()
 
 
-@r.post("/ingest/{new_data}", response_model=str, status_code=status.HTTP_201_CREATED)
-async def ingest_data(request: Request, new_data: str) -> str:
+@r.post("/ingest", response_model=str, status_code=status.HTTP_201_CREATED)
+async def ingest_data(request: Request, new_data: dict[str, Any]) -> str:
 
+    print(new_data)
     return "Hello world"
--- tests/unit_tests/routers/ingest/test_ingest.py
+++ tests/unit_tests/routers/ingest/test_ingest.py
@@ -10,12 +10,14 @@ from fastapi.testclient import TestClient
 
 def test_ingest_when_not_authenticated(client: TestClient):
     response = client.post(
-        "/api/v1/ingest/test",
+        "/api/v1/ingest",
     )
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 def test_create_when_ok(client: TestClient, user_header_token):
 
-    response = client.post("/api/v1/ingest/somestring", headers=user_header_token)
+    response = client.post(
+        "/api/v1/ingest", json={"test": "data"}, headers=user_header_token
+    )
     assert response.status_code == status.HTTP_201_CREATED
