bca38a5f9f3e74bcb60889bf4b7dcfc761111855
Anna Pokorska <anna@climatepolicyradar.org>
2024-08-13 11:32:55 +0100

    Return collection template

--- app/api/api_v1/routers/ingest.py
+++ app/api/api_v1/routers/ingest.py
@@ -2,6 +2,7 @@ import logging
 
 from fastapi import APIRouter, UploadFile, status
 
+from app.model.collection import CollectionCreateDTO
 from app.model.general import Json
 
 ingest_router = r = APIRouter()
@@ -21,8 +22,15 @@ async def get_ingest_template(corpus_type: str) -> Json:
     :param str corpus_type: type of the corpus of data to ingest.
     :return Json: json representation of ingest template.
     """
+
+    collection_schema = CollectionCreateDTO.model_json_schema(mode="serialization")
+    collection_properties = collection_schema["properties"]
+
+    collection_template = {x: "" for x in collection_properties}
+
     _LOGGER.info(corpus_type)
-    return {"hello": "world"}
+
+    return collection_template
 
 
 @r.post("/ingest", response_model=Json, status_code=status.HTTP_201_CREATED)
