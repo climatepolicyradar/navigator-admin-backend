b721f5693f2e0faaaea2e41e3570632ba3e7e177
annaCPR <anna@climatepolicyradar.org>
2024-09-26 10:09:50 +0100

    Feature/pdct 1499 add a limit to how many documents can be ingested in one go (#215)
    * Remove comments
    
    * Correct typo and move tests around
    
    * Do not save documents over ingest limit + update repo mocks
    
    * Add idempotency when saving documents and families
    
    * Refactor
    
    * Refactor
    
    * Add idempotency for saving events
    
    * Add idempotency when saving collections
    
    * Refactor to remove duplication
    
    * Bump minor version
    
    * Update check-auto-tagging-will-work job in git workflows
    
    * Update python-precommit-validator in git workflows
    
    * Update python-precommit-validator to latest
    
    * Fix tests
    
    * Add db assertions to the integration test

--- tests/unit_tests/service/ingest/test_ingest_service.py
+++ tests/unit_tests/service/ingest/test_ingest_service.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from unittest.mock import Mock, patch
 
 import pytest
 
@@ -6,6 +7,7 @@ import app.service.ingest as ingest_service
 from app.errors import RepositoryError, ValidationError
 
 
+@patch("app.service.ingest._exists_in_db", Mock(return_value=False))
 def test_ingest_when_ok(
     corpus_repo_mock,
     geography_repo_mock,
@@ -58,12 +60,13 @@ def test_ingest_when_ok(
 
     assert {
         "collections": ["test.new.collection.0"],
-        "families": ["created"],
-        "documents": ["test.new.doc.0"],
+        "families": ["test.new.family.0"],
+        "documents": ["test.new.document.0"],
         "events": ["test.new.event.0"],
     } == ingest_service.import_data(test_data, "test")
 
 
+@patch("app.service.ingest._exists_in_db", Mock(return_value=False))
 def test_ingest_when_db_error(corpus_repo_mock, collection_repo_mock):
     collection_repo_mock.throw_repository_error = True
 
@@ -101,31 +104,6 @@ def test_save_families_when_data_invalid(corpus_repo_mock, validation_service_mo
     assert "Error" == e.value.message
 
 
-# TODO: Uncomment when implementing feature/pdct-1402-validate-collection-exists-before-creating-family
-# def test_ingest_families_when_collection_ids_do_not_exist(
-#     corpus_repo_mock, geography_repo_mock, collection_repo_mock
-# ):
-#     collection_repo_mock.missing = True
-
-#     test_data = {
-#         "families": [
-#             {
-#                 "import_id": "test.new.family.0",
-#                 "title": "Test",
-#                 "summary": "Test",
-#                 "geography": "Test",
-#                 "category": "UNFCCC",
-#                 "metadata": {},
-#                 "collections": ["id.does.not.exist"],
-#             },
-#         ],
-#     }
-#     with pytest.raises(ValidationError) as e:
-#         ingest_service.import_data(test_data, "test")
-#     expected_msg = "One or more of the collections to update does not exist"
-#     assert e.value.message == expected_msg
-
-
 def test_save_documents_when_data_invalid(validation_service_mock):
     validation_service_mock.throw_validation_error = True
 
@@ -133,6 +111,60 @@ def test_save_documents_when_data_invalid(validation_service_mock):
 
     with pytest.raises(ValidationError) as e:
         ingest_service.save_documents(test_data, "test")
+    assert "Error" == e.value.message
+
+
+@patch("app.service.ingest._exists_in_db", Mock(return_value=False))
+def test_do_not_save_documents_over_ingest_limit(
+    validation_service_mock, document_repo_mock, monkeypatch
+):
+    monkeypatch.setattr(ingest_service, "DOCUMENT_INGEST_LIMIT", 1)
+
+    test_data = [
+        {
+            "import_id": "test.new.document.0",
+            "family_import_id": "test.new.family.0",
+            "variant_name": "Original Language",
+            "metadata": {"color": ["blue"]},
+            "title": "",
+            "source_url": None,
+            "user_language_name": "",
+        },
+        {
+            "import_id": "test.new.document.1",
+            "family_import_id": "test.new.family.1",
+            "variant_name": "Original Language",
+            "metadata": {"color": ["blue"]},
+            "title": "",
+            "source_url": None,
+            "user_language_name": "",
+        },
+    ]
+
+    saved_documents = ingest_service.save_documents(test_data, "test")
+    assert ["test.new.document.0"] == saved_documents
+
+
+def test_ingest_documents_when_no_family():
+    fam_import_id = "test.new.family.0"
+    test_data = {
+        "documents": [
+            {"import_id": "test.new.document.0", "family_import_id": fam_import_id}
+        ]
+    }
+
+    with pytest.raises(ValidationError) as e:
+        ingest_service.import_data(test_data, "test")
+    assert f"No entity with id {fam_import_id} found" == e.value.message
+
+
+def test_save_events_when_data_invalid(validation_service_mock):
+    validation_service_mock.throw_validation_error = True
+
+    test_data = [{"import_id": "invalid"}]
+
+    with pytest.raises(ValidationError) as e:
+        ingest_service.save_events(test_data, "test")
     assert "Error" == e.value.message
 
 
@@ -169,26 +201,3 @@ def test_validate_entity_relationships_when_no_collection_matching_family():
     with pytest.raises(ValidationError) as e:
         ingest_service.validate_entity_relationships(test_data)
     assert f"No entity with id {coll_import_id} found" == e.value.message
-
-
-def test_save_documents_when_no_family():
-    fam_import_id = "test.new.family.0"
-    test_data = {
-        "documents": [
-            {"import_id": "test.new.document.0", "family_import_id": fam_import_id}
-        ]
-    }
-
-    with pytest.raises(ValidationError) as e:
-        ingest_service.import_data(test_data, "test")
-    assert f"No entity with id {fam_import_id} found" == e.value.message
-
-
-def test_save_events_when_data_invalid(validation_service_mock):
-    validation_service_mock.throw_validation_error = True
-
-    test_data = [{"import_id": "imvalid"}]
-
-    with pytest.raises(ValidationError) as e:
-        ingest_service.save_events(test_data, "test")
-    assert "Error" == e.value.message
