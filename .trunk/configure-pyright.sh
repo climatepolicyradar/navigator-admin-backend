b2f91cdf66d77ade0a354c83d4097191e62b8658
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-11-13 15:57:09 +0000

    Update configure-pyright.sh

--- .trunk/configure-pyright.sh
+++ .trunk/configure-pyright.sh
@@ -7,22 +7,10 @@ venv_name=$(grep -m 1 venv pyproject.toml | tr -s ' ' | tr -d '"' | tr -d "'" | 
 # Check if pyrightconfig already exists.
 if [[ ! -f pyrightconfig.json ]]; then
 
-	if [[ -z ${venv_name} ]]; then
-		echo "Using Poetry"
-		poetry_env_info=$(poetry env info --path) >/dev/null 2>&1
-		venv_path=${poetry_env_info%/*} # ABCDE
-		venv_name=${poetry_env_info#*/} # 12345
-
-		# Generate the pyrightconfig.json file.
-		json_string=$(jq -n \
-			--arg v "${venv_name}" \
-			--arg vp "${venv_path}" \
-			'{venv: $v, venvPath: $vp} ')
-
-		echo "${json_string}" >pyrightconfig.json
-	else
-		echo "Using Pyenv"
-		# Check if pyenv-pyright plugin is installed
+	# Capture poetry environment path and check if it's empty.
+	poetry_env_info=$(poetry env info --path 2>/dev/null || true)
+	if [[ -z ${poetry_env_info} ]]; then
+		# Check if pyenv is installed
 		if ! command -v pyenv &>/dev/null; then
 			echo "pyenv not installed. Please install pyenv..."
 			exit 1
@@ -32,7 +20,7 @@ if [[ ! -f pyrightconfig.json ]]; then
 		dir_path="${pyenv_root}"/plugins/pyenv-pyright
 		if [[ ! -d ${dir_path} ]]; then
 			# trunk-ignore(shellcheck/SC2312)
-			if [[ -n $(ls -A "${dir_path}") ]]; then
+			if [[ -z $(ls -A "${dir_path}") ]]; then
 				git clone https://github.com/alefpereira/pyenv-pyright.git "${dir_path}"
 			fi
 		fi
@@ -40,6 +28,18 @@ if [[ ! -f pyrightconfig.json ]]; then
 		# Generate the pyrightconfig.json file.
 		pyenv pyright "${venv_name}"
 		pyenv local "${venv_name}"
+	else
+		poetry_env_info=$(poetry env info --path 2>/dev/null || true)
+		venv_path=$(dirname "${poetry_env_info}")  # Get directory path
+		venv_name=$(basename "${poetry_env_info}") # Get base name
+
+		# Generate the pyrightconfig.json file.
+		json_string=$(jq -n \
+			--arg v "${venv_name}" \
+			--arg vp "${venv_path}" \
+			'{venv: $v, venvPath: $vp} ')
+
+		echo "${json_string}" >pyrightconfig.json
 	fi
 fi
 
@@ -48,5 +48,4 @@ if ! jq -r --arg venv_name "${venv_name}" '. | select((.venv != $venv_name or .v
 	echo "Failed to configure pyright to use environment '${venv_name}' as interpreter. Please check pyrightconfig.json..."
 	exit 1
 fi
-echo "All done!"
 exit 0
