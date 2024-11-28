26b6c8feef7bf1d22546e9d697a01319f4c6bcdc
Anna Pokorska <anna@climatepolicyradar.org>
2024-11-28 15:04:35 +0000

    Add error handling

--- tests/integration_tests/init-s3.py
+++ tests/integration_tests/init-s3.py
@@ -1,12 +1,18 @@
+import logging
 import os
 
 import boto3
 
-s3_client = boto3.client(
-    "s3",
-    endpoint_url=os.environ["AWS_ENDPOINT_URL"],
-    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-)
+_LOGGER = logging.getLogger(__name__)
 
-s3_client.create_bucket(Bucket=os.environ["BULK_IMPORT_BUCKET"])
+try:
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=os.environ["AWS_ENDPOINT_URL"],
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+
+    s3_client.create_bucket(Bucket=os.environ["BULK_IMPORT_BUCKET"])
+except Exception as e:
+    _LOGGER.error(e)
