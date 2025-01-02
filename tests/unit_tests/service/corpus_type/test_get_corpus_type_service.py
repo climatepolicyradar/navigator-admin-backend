4bc2f976043ab3549c5ca83b499d2cf6714de630
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-01-02 13:16:57 +0000

    Add service unit tests

---
+++ tests/unit_tests/service/corpus_type/test_get_corpus_type_service.py
@@ -0,0 +1,15 @@
+import app.service.corpus_type as corpus_type_service
+
+
+def test_get(corpus_type_repo_mock):
+    result = corpus_type_service.get("some_corpus_type_name")
+    assert result is not None
+    assert result.name == "test_name"
+    assert corpus_type_repo_mock.get.call_count == 1
+
+
+def test_get_returns_none(corpus_type_repo_mock):
+    corpus_type_repo_mock.return_empty = True
+    result = corpus_type_service.get("some_corpus_type_name")
+    assert result is not None
+    assert corpus_type_repo_mock.get.call_count == 1
