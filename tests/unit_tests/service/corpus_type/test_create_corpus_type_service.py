13e60ed148f335d52c039da155e87048fa03f7d1
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-01-02 16:23:20 +0000

    Update test_create_corpus_type_service.py

--- tests/unit_tests/service/corpus_type/test_create_corpus_type_service.py
+++ tests/unit_tests/service/corpus_type/test_create_corpus_type_service.py
@@ -5,17 +5,17 @@ from app.errors import RepositoryError
 from tests.helpers.corpus_type import create_corpus_type_create_dto
 
 
-def test_create(corpus_type_repo_mock, admin_user_context):
+def test_create(corpus_type_repo_mock):
     new_ct = create_corpus_type_create_dto()
-    ct = corpus_type_service.create(new_ct, admin_user_context.org_id)
+    ct = corpus_type_service.create(new_ct)
     assert ct is not None
     assert corpus_type_repo_mock.create.call_count == 1
 
 
-def test_create_when_db_fails(corpus_type_repo_mock, admin_user_context):
+def test_create_when_db_fails(corpus_type_repo_mock):
     new_ct = create_corpus_type_create_dto()
     corpus_type_repo_mock.throw_repository_error = True
     with pytest.raises(RepositoryError) as e:
-        corpus_type_service.create(new_ct, admin_user_context.org_id)
+        corpus_type_service.create(new_ct)
     assert e.value.message == "bad corpus type repo"
     assert corpus_type_repo_mock.create.call_count == 0
