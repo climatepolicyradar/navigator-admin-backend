592fc366fdb8e4699e48fd11e347f44b97ce3fd4
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-01-02 17:03:08 +0000

    Update user token

--- tests/unit_tests/routers/corpus_type/test_create.py
+++ tests/unit_tests/routers/corpus_type/test_create.py
@@ -5,25 +5,25 @@ from tests.helpers.corpus_type import create_corpus_type_create_dto
 
 
 def test_create_when_ok(
-    client: TestClient, corpus_type_service_mock, user_header_token
+    client: TestClient, corpus_type_service_mock, superuser_header_token
 ):
-    new_data = create_corpus_type_create_dto("test").model_dump(mode="json")
+    new_data = create_corpus_type_create_dto("test_ct_name").model_dump(mode="json")
     response = client.post(
-        "/api/v1/corpus-types", json=new_data, headers=user_header_token
+        "/api/v1/corpus-types", json=new_data, headers=superuser_header_token
     )
     assert response.status_code == status.HTTP_201_CREATED
     data = response.json()
-    assert data == "test"
+    assert data == "test_ct_name"
     assert corpus_type_service_mock.create.call_count == 1
 
 
 def test_create_fails_when_validation_error(
-    client: TestClient, corpus_type_service_mock, user_header_token
+    client: TestClient, corpus_type_service_mock, superuser_header_token
 ):
     corpus_type_service_mock.throw_validation_error = True
     new_data = create_corpus_type_create_dto("validation_error").model_dump(mode="json")
     response = client.post(
-        "/api/v1/corpus-types", json=new_data, headers=user_header_token
+        "/api/v1/corpus-types", json=new_data, headers=superuser_header_token
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     data = response.json()
