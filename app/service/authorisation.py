1aed92fad576de9da0e771b65580f79e3392382e
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-01-02 11:53:24 +0000

    Update authorisation.py

--- app/service/authorisation.py
+++ app/service/authorisation.py
@@ -70,7 +70,8 @@ def is_authorised(user: UserContext, entity: AuthEndpoint, op: AuthOperation) ->
     if _has_access(required_access, _get_user_access(user)):
         return
 
-    _LOGGER.error(entity, entity.value)
+    _LOGGER.error(entity)
+    _LOGGER.error(entity.value)
 
     raise AuthorisationError(
         f"User {user.email} is not authorised to {op} {_get_article(entity.value)} {entity}"
