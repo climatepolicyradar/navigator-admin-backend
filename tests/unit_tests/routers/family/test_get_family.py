620581ed5dad99a436d32d096fee8d0eb877262b
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2025-01-06 14:09:08 +0000

    test: update tests to include multi geos

--- tests/unit_tests/routers/family/test_get_family.py
+++ tests/unit_tests/routers/family/test_get_family.py
@@ -12,6 +12,7 @@ def test_get_all_when_ok(client: TestClient, family_service_mock, user_header_to
     response = client.get("/api/v1/families", headers=user_header_token)
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
+
     assert isinstance(data, list)
     assert len(data) > 0
     assert data[0]["import_id"] == "test"
@@ -33,3 +34,19 @@ def test_get_when_not_found(client: TestClient, family_service_mock, user_header
     data = response.json()
     assert data["detail"] == "Family not found: fam1"
     assert family_service_mock.get.call_count == 1
+
+
+def test_get_endpoint_returns_families_with_multiple_geographies(
+    client: TestClient, family_service_mock, user_header_token
+):
+    response = client.get("/api/v1/families", headers=user_header_token)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) == 1
+    assert family_service_mock.all.call_count == 1
+    for item in data:
+        keys = item.keys()
+        assert "geography" in keys
+        assert "geographies" in keys
+        assert item["geographies"] == ["CHN", "BRB", "BHS"]
