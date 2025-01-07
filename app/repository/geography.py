6784dd76b59b7e6053f78ac0fcd48024b7b962e5
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2025-01-07 13:39:11 +0000

    feat: add function to retrieve list of geo ids from iso codes
    - function that retrieves the list of geo ids from the geo repo, based
      on the iso codes thats provided in the payload. It validates these iso
      codes at the same time returning an error if the geography does not
      exist in the db

--- app/repository/geography.py
+++ app/repository/geography.py
@@ -5,4 +5,25 @@ from sqlalchemy.orm import Session
 
 
 def get_id_from_value(db: Session, geo_string: str) -> Optional[int]:
+    """
+    Fetch the ID of a geography based on its iso value.
+
+    :param Session db: Database session.
+    :param str geo_string: The geography value to look up.
+    :return Optional[int]: The ID of the geography if found, otherwise None.
+    """
     return db.query(Geography.id).filter_by(value=geo_string).scalar()
+
+
+def get_ids_from_values(db: Session, geo_strings: list[str]) -> list[int]:
+    """
+    Fetch IDs for multiple geographies based on their iso values.
+
+    :param Session db: Database session.
+    :param list[str] geo_strings: A list of geography iso values to look up.
+    :return list[int]: A list of IDs corresponding to the provided geography values.
+    """
+    return [
+        geography.id
+        for geography in db.query(Geography).filter(Geography.value.in_(geo_strings))
+    ]
--- tests/mocks/repos/geography_repo.py
+++ tests/mocks/repos/geography_repo.py
@@ -8,6 +8,13 @@ def mock_geography_repo(geography_repo, monkeypatch: MonkeyPatch, mocker):
         if not geography_repo.error:
             return 1
 
+    def mock_get_ids_from_values(_, __) -> list[int]:
+        if not geography_repo.error:
+            return [1, 2]
+        return []
+
     geography_repo.error = False
+    monkeypatch.setattr(geography_repo, "get_ids_from_values", mock_get_ids_from_values)
     monkeypatch.setattr(geography_repo, "get_id_from_value", mock_get_id_from_value)
     mocker.spy(geography_repo, "get_id_from_value")
+    mocker.spy(geography_repo, "get_ids_from_values")
