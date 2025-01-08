a83ffd76bd6fc6d10c7cadeb93eb1a3195670c85
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2025-01-08 10:55:18 +0000

    test: integration tests for update endpoint with invalid geo id in list

--- tests/integration_tests/family/test_update.py
+++ tests/integration_tests/family/test_update.py
@@ -549,3 +549,28 @@ def test_update_family_metadata_if_changed(
         .one()
     )
     assert metadata.value == expected_meta
+
+
+def test_update_family_returns_error_if_payload_contains_invalid_geography_ids(
+    client: TestClient, data_db: Session, user_header_token
+):
+    setup_db(data_db)
+    response = client.get(
+        "/api/v1/families/A.0.0.2",
+        headers=user_header_token,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    family_data = response.json()
+    assert family_data["geography"] == "ZWE"
+
+    family_data["geographies"] = ["AGO", "ZWE", "ABC"]
+    response = client.put(
+        "/api/v1/families/A.0.0.2", json=family_data, headers=user_header_token
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    data = response.json()
+    assert (
+        data["detail"]
+        == "One or more of the following geography values are invalid: AGO, ZWE, ABC"
+    )
