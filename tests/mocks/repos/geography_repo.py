ef8e218cfc4aba8af5ebb5750b3c1aecb6c29f41
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2025-01-09 12:47:06 +0000

    tests: add tests for repository errors

--- tests/mocks/repos/geography_repo.py
+++ tests/mocks/repos/geography_repo.py
@@ -2,13 +2,28 @@ from typing import Optional
 
 from pytest import MonkeyPatch
 
+from app.errors import RepositoryError
+
 
 def mock_geography_repo(geography_repo, monkeypatch: MonkeyPatch, mocker):
+    geography_repo.return_empty = False
+    geography_repo.failed_to_remove_geography = False
+    geography_repo.failed_to_add_geography = False
+
+    def maybe_throw():
+        if geography_repo.failed_to_remove_geography:
+            raise RepositoryError("Failed to remove geography from family")
+        if geography_repo.failed_to_add_geography:
+            raise RepositoryError("Failed to add geography to family")
+
     def mock_get_id_from_value(_, __) -> Optional[int]:
+        maybe_throw()
         if not geography_repo.error:
             return 1
+        return None
 
     def mock_get_ids_from_values(_, __) -> list[int]:
+        maybe_throw()
         if not geography_repo.error:
             return [1, 2]
         return []
