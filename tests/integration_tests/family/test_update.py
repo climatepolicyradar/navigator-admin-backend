196a92d84fb77e6c7a2080451aff20486962c7e2
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2025-01-16 09:30:18 +0000

    fix: failing integration tests

--- tests/integration_tests/family/test_update.py
+++ tests/integration_tests/family/test_update.py
@@ -611,32 +611,29 @@ def test_update_endpoint_handles_multiple_updates(
         "/api/v1/families/A.0.0.2",
         headers=user_header_token,
     )
-
     assert response.status_code == status.HTTP_200_OK
     family_data = response.json()
     assert family_data["geographies"] == ["ZWE"]
+    family_data["geographies"] = ["AGO", "ZWE"]
+    family_data["title"] = "Updated Title"
 
-    family_data["geographies"] = ["MYS", "ZWE"]
-    family_data["title"] = "Title for Malaysia and Zimbabwe"
-
-    update_endpoint_response = client.put(
+    response = client.put(
         "/api/v1/families/A.0.0.2", json=family_data, headers=user_header_token
     )
 
-    assert update_endpoint_response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_200_OK
     data = response.json()
+    assert data["geographies"] == ["AGO", "ZWE"]
+    assert data["title"] == "Updated Title"
 
-    assert data["geographies"] == ["MYS", "ZWE"]
-    assert data["title"] == "Title for Malaysia and Zimbabwe"
+    family_data["geographies"] = ["AGO", "ZWE", "USA"]
+    family_data["title"] = "Updated Title for Angola, Zimbabwe and USA"
 
-    family_data["tite"] = "Title for the countries of Malaysia and Zimbabwe and USA"
-    family_data["geographies"] = ["MYS", "ZWE", "USA"]
-
-    update_endpoint_response = client.put(
+    response = client.put(
         "/api/v1/families/A.0.0.2", json=family_data, headers=user_header_token
     )
 
-    assert update_endpoint_response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert data["geographies"] == ["MYS", "ZWE", "USA"]
-    assert data["title"] == "Title for the countries of Malaysia and Zimbabwe and USA"
+    assert data["geographies"] == ["AGO", "ZWE", "USA"]
+    assert data["title"] == "Updated Title for Angola, Zimbabwe and USA"
