f8d347da2c7c0e45820f18f78e83d973b342eab5
Anna Pokorska <anna@climatepolicyradar.org>
2025-03-17 16:23:13 +0000

    Update bulk import relationship validation to return a list of missing entities rather than error on the first found

--- tests/unit_tests/routers/bulk_import/test_bulk_import.py
+++ tests/unit_tests/routers/bulk_import/test_bulk_import.py
@@ -4,7 +4,6 @@ from unittest.mock import Mock, patch
 
 from fastapi import status
 from fastapi.testclient import TestClient
-
 from tests.helpers.bulk_import import (
     build_json_file,
     default_collection,
@@ -120,4 +119,4 @@ def test_bulk_import_documents_when_no_family(
     )
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json().get("detail") == "No entity with id test.new.family.0 found"
+    assert response.json().get("detail") == "Missing entities: ['test.new.family.0']"
