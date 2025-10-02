5c565ccc14d9c18e42cdd74f21e69255307d3f1e
odrakes-cpr <osneil@climatepolicyradar.org>
2025-10-02 14:32:18 +0100

    feat: surface the attribution urls on the corpus endpoint (#391)
    * feat: surface the attribution urls on the corpus endpoint
    
    * fix: remove rogue breakpoint;
    
    ---------
    
    Co-authored-by: Osneil Drakes <osneildrakes@MacBookPro.communityfibre.co.uk>

--- tests/helpers/corpus.py
+++ tests/helpers/corpus.py
@@ -9,6 +9,7 @@ def create_corpus_write_dto(
     description: Optional[str] = "description",
     image_url: Optional[str] = "some-picture.png",
     corpus_type_description: str = "some description",
+    attribution_url: Optional[str] = None,
 ) -> CorpusWriteDTO:
     return CorpusWriteDTO(
         title=title,
@@ -16,6 +17,7 @@ def create_corpus_write_dto(
         corpus_text=corpus_text,
         corpus_image_url=image_url,
         corpus_type_description=corpus_type_description,
+        attribution_url=attribution_url,
     )
 
 
@@ -27,6 +29,7 @@ def create_corpus_create_dto(
     image_url: Optional[str] = "some-picture.png",
     org_id: int = 1,
     import_id: Optional[str] = None,
+    attribution_url: Optional[str] = None,
 ) -> CorpusCreateDTO:
     return CorpusCreateDTO(
         import_id=import_id,
@@ -36,4 +39,5 @@ def create_corpus_create_dto(
         corpus_image_url=image_url,
         organisation_id=org_id,
         corpus_type_name=corpus_type,
+        attribution_url=attribution_url,
     )
