ecd0df177e9bdba31d0956f09a47aa614f6a0f51
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-11-04 11:34:01 +0000

    Revert "Update setup_db.py"
    This reverts commit fa47adfaa93d1fb214cf7a314fb2abb1bad82212.

--- tests/integration_tests/setup_db.py
+++ tests/integration_tests/setup_db.py
@@ -24,8 +24,7 @@ from db_client.models.document.physical_document import (
 )
 from db_client.models.organisation import Corpus, EntityCounter
 from db_client.models.organisation.users import AppUser, Organisation, OrganisationUser
-
-# from sqlalchemy import update
+from sqlalchemy import update
 from sqlalchemy.orm import Session
 
 
@@ -519,17 +518,17 @@ def _setup_organisation(test_db: Session) -> tuple[int, int]:
 
 
 def setup_corpus(test_db: Session) -> None:
-    pass
-    # test_db.execute(
-    #     update(EntityCounter).values(
-    #         counter=1,
-    #     )
-    # )
-    # test_db.commit()
-    # print(test_db.query(EntityCounter).all())
+    print(test_db.query(EntityCounter).all())
 
-    # for item in test_db.query(EntityCounter.counter).all():
-    #     assert item[0] == 1
+    test_db.execute(
+        update(EntityCounter).values(
+            counter=1,
+        )
+    )
+    test_db.commit()
+
+    for item in test_db.query(EntityCounter.counter).all():
+        assert item[0] == 1
 
 
 def _setup_collection_data(
